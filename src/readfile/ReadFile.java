1b02002c8f34dba683d0d3269187d0ac9e388667
Morteza <Morteza@192.168.1.5>
2018-10-16 23:30:11 +0330

    reading file added

--- src/readfile/ReadFile.java
+++ src/readfile/ReadFile.java
@@ -1,5 +1,40 @@
 package readfile;
 
-public class ReadFile {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
+public class ReadFile {
+	private String fileName;
+
+	public ReadFile(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public int[][] readFileInArray() {
+		List<String> list = Collections.emptyList();
+		try {
+			list = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		int[][] arr = returnMatrix(list);
+		return arr;
+	}
+
+	private int[][] returnMatrix(List<String> list) {
+		int size = list.size();
+		int[][] arr = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			String[] temp = list.get(i).split(" ");
+			for (int j = 0; j < temp.length; j++) {
+				arr[i][j] = Integer.valueOf(temp[j]);
+			}
+		}
+		return arr;
+	}
 }
